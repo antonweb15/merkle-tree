179d27986ed19b3eb3f79dcdca59ee61e43f8ccd
antonweb15 <antonweb15@gmail.com>
2025-09-18 12:48:34 +0700

    2-nd step(Client)

--- client/index.js
+++ client/index.js
@@ -5,12 +5,12 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  const name = niceList[0]; // или выберите своё имя из списка
+  const tree = new MerkleTree(niceList);
+  const index = niceList.findIndex(n => n === name);
+  const proof = tree.getProof(index);
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
-
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, { name, proof });
   console.log({ gift });
 }
 
